08aa5cb638c9177ab012f28fa6e997ed459e7dda
lexor88 <maksimleksin88@gmail.com>
2024-04-01 20:08:20 +0300

    dz 13.2, first commit

--- src/category.py
+++ src/category.py
@@ -9,7 +9,18 @@ class Category:
     def __init__(self, name: str, description: str, products: list[Product]):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1
         Category.products_count += len(products)
+
+    def add_product(self, new_product):
+        self.__products.append(new_product)
+
+    @property
+    def products(self):
+        list_products = ''
+        for product in self.__products:
+            product_for_append = f"{product.name}, {product.price} руб. Остаток: {product.quantity}\n"
+            list_products += product_for_append
+        return list_products
